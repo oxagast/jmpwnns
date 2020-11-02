2aea68cb29d05ae620eb1fcb6a01fe76f985de78
Marshall Lee Whittaker <marshall@jinkon.maskster.site>
2020-11-02 02:38:36 -0500

    Retooled some stuff to and took out unecessary stuff.

--- nnetwork.cpp
+++ nnetwork.cpp
@@ -9,35 +9,27 @@
 //  (  O )  (/    ( (_ /    \___ \ )(
 //   \__(_/\_\_/\_/\___\_/\_(____/(__)
 //
-//  g++ nw.cpp teach.cpp common.cpp -lm -o nn
+//  gcc main.cpp -o main -lm
 
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define LEARNRATE 0.05
-#define LEARNLOOPS 20000
+#define LEARNRATE 0.25
+#define LEARNLOOPS 50000
 
-void train_csv();
-void train_xor();
-void train_and();
-void train_nand();
-void train_nor();
-void train_or();
-void train_not();
-void train_xnor();
+
+float sigmoid (float sigme) {
+  //printf("%f", sigme);
+  float s = 1.0 / (1.0 + exp(sigme * -1)); 
+  return(s);
+}
+
 
 float weight[] = {0.15, 0.20, 0.25, 0.30, 0.40, 0.45, 0.50, 0.55};
 float bias_a = 0.35;
 float bias_b = 0.60;
 float net_err_per[1];
-
-float sigmoid(float sigme) {
-  //sigmoidal squashing function
-  float s = 1.0 / (1.0 + exp(sigme * -1));
-  return (s);
-}
 
 float calc_it(float ina, float inb, float f_weight, float s_weight,
               float bias) {
@@ -50,13 +42,17 @@ struct Tuple {
 };
 
 struct Tuple network(float tf_a, float tf_b, float tt_a, float tt_b) {
-  struct Tuple n;
-//  printf("Trying for: %0.0f %0.0f\n", tt_a, tt_b);
-//  printf("Inputs to neurons are: %f %f\n", tf_a, tf_b);
+  struct Tuple tuple;
+  printf("Trying for: %f, %f\n", tt_a, tt_b);
+  printf("Inputs to neurons are: %f, %f\n", tf_a, tf_b);
+  // learning rate
+  // float lr = 0.5;
+  // network error declare
+  float net_err_per[2];
   // first pass
-  float out[3];
-  float o_out[3];
-//  for (int ll; ll < LEARNLOOPS; ll++) { // learn loop
+  float out[2];
+  float o_out[2];
+  for (int ll; ll < LEARNLOOPS; ll++) { // learn loop
     out[1] = sigmoid(calc_it(tf_a, tf_b, weight[0], weight[1], bias_a));
     out[2] = sigmoid(calc_it(tf_a, tf_b, weight[2], weight[3], bias_a));
     // calculate out back through
@@ -65,8 +61,8 @@ struct Tuple network(float tf_a, float tf_b, float tt_a, float tt_b) {
     // printf("OUT1: %f OUT2: %f OUTo1: %f OUTo2: %f\n", out[1], out[2],
     // o_out[1], o_out[2]);
     // calculate the network error
-    net_err_per[0] = 0.5 * pow((tt_a - o_out[1]), 2);
-    net_err_per[1] = 0.5 * pow((tt_b - o_out[2]), 2);
+    net_err_per[1] = 0.5 * pow((tt_a - o_out[1]), 2);
+    net_err_per[2] = 0.5 * pow((tt_b - o_out[2]), 2);
     // target - output
     float tar_less_out[2];
     tar_less_out[1] = (o_out[1] - tt_a);
@@ -75,13 +71,13 @@ struct Tuple network(float tf_a, float tf_b, float tt_a, float tt_b) {
     // derrivitive
     der[1] = o_out[1] * (1 - o_out[1]);
     der[2] = o_out[2] * (1 - o_out[2]);
-    float diff[9];
-    float bp[3];
+    float diff[8];
+    float bp[2];
     // delta total
-    float total[3];
+    float total[2];
     total[1] = tar_less_out[1] * der[1] * out[1];
     total[2] = tar_less_out[2] * der[2] * out[2];
-    float nweight[8];
+    float nweight[7];
     // for each weight above 4
     nweight[4] = weight[4] - LEARNRATE * total[1];
     nweight[5] = weight[5] - LEARNRATE * total[1];
@@ -90,12 +86,12 @@ struct Tuple network(float tf_a, float tf_b, float tt_a, float tt_b) {
     // start the back prop
     bp[1] = tar_less_out[1] * der[1];
     bp[2] = tar_less_out[2] * der[2];
-    float p[3];
+    float p[2];
     p[1] = bp[1] * weight[4];
     p[2] = bp[2] * weight[4];
     // printf("%f %f %f %f\n", nweight[4], nweight[5], nweight[6], nweight[7]);
-    float hns[3];
-    float hns_p[3];
+    float hns[2];
+    float hns_p[2];
     hns[1] = tar_less_out[1] * der[1];
     hns[2] = tar_less_out[2] * der[2];
     hns_p[1] = hns[1] * weight[4];
@@ -115,39 +111,51 @@ struct Tuple network(float tf_a, float tf_b, float tt_a, float tt_b) {
     weight[5] = nweight[5];
     weight[6] = nweight[6];
     weight[7] = nweight[7];
-//}
-  n = {o_out[1], o_out[2]};
-  return(n);
-}
-
-
-void reset_weight() {
-  weight[0] = 15;
-  weight[1] = 20;
-  weight[2] = 25;
-  weight[3] = 30;
-  weight[4] = 40;
-  weight[5] = 45;
-  weight[5] = 50;
-  weight[6] = 55;
+  }
+  //printf("Network Error: A: %f  -  B: %f \n", net_err_per[1], net_err_per[2]);
+  //printf("Output: A:%f  -  B: %f\n", o_out[1], o_out[2]);
+  tuple = {o_out[1], o_out[2]};
+  // reset the weights for next rounds!
+  weight[0] = 0.15;
+  weight[1] = 0.20;
+  weight[2] = 0.25;
+  weight[3] = 0.30;
+  weight[4] = 0.40;
+  weight[5] = 0.45;
+  weight[6] = 0.50;
+  weight[7] = 0.55;
+  return(tuple);
 }
 
 int main() {
-  // predict
-  struct Tuple prediction;
-  float pin[2];
-  pin[0] = 1;
-  pin[1] = 0;
-  for(int i; i < LEARNLOOPS; i++) {
-    train_xor();
-    reset_weight();
-  }
-  prediction = network(pin[0], pin[1], 0,0);
-  printf("Prediction from %0.0f %0.0f: %0.0f %0.0f\n", 
-		  pin[0], pin[1], prediction.out_o_a, prediction.out_o_b);
+  float out_o_a, out_o_b;
+  struct Tuple tuple;
+
+  // lets try to teach it XOR!!
+  tuple = network(0,0,0,0);
+  out_o_a = tuple.out_o_a;
+  out_o_b = tuple.out_o_b;
+  printf("%f %f\n", out_o_a, out_o_b);
+  tuple = network(0,1,1,1);
+  out_o_a = tuple.out_o_a;
+  out_o_b = tuple.out_o_b;
+  printf("%f %f\n", out_o_a, out_o_b);
+  tuple = network(1,0,1,1);
+  out_o_a = tuple.out_o_a;
+  out_o_b = tuple.out_o_b;
+  printf("%f %f\n", out_o_a, out_o_b);
+  tuple = network(1,1,0,0);
+  out_o_a = tuple.out_o_a;
+  out_o_b = tuple.out_o_b;
+  printf("%f %f\n", out_o_a, out_o_b);
+
   printf("Final Weights:\nw1 %f w2 %f w3 %f w4 %f\nw5 %f w6 %f w7 %f w8 %f\n", 
-		  weight[0], weight[1], weight[2], weight[3], weight[4],
-		  weight[5], weight[6], weight[7]);
+      weight[0], weight[1], weight[2], weight[3], weight[4],
+      weight[5], weight[6], weight[7]);
   printf("Network Error: A: %f  -  B: %f \n", net_err_per[0], net_err_per[1]);
   return(0);
+
+
+  return(0);
 }
+
